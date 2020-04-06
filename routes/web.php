2004f63392736d26f886a29d4301bf6a31b5ac51
Ridwan Andalus <ridwan.andalus79@gmail.com>
2020-04-06 22:08:57 +0700

    Done

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layout-success');
+    return view('index');
 });
 
 // Route::get('event', 'WeddingController@event')->name('event');
